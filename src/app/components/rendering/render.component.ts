221fbeb07be2b80abd1ee4c36859f4f8a181f9dd
Dimitri Zimmermann <dimitri.zimmermann@rwth-aachen.de>
2019-08-14 00:01:49 +0200

    Refactorign

--- src/app/components/rendering/render.component.ts
+++ src/app/components/rendering/render.component.ts
@@ -1,17 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FabscanComponent} from "../fabscan.component";
 
-const LEFT_MOUSE_BUTTON:   number = 0;
-const MIDDLE_MOUSE_BUTTON: number = 1;
-const RIGHT_MOUSE_BUTTON:  number = 2;
-
-interface MouseState
-{
-  leftButtonDown: boolean;
-  rightButtonDown: boolean;
-  middleButtonDown: boolean;
-}
-
 @Component({
   selector: 'fabscan-renderer',
   templateUrl: './render.component.html',
